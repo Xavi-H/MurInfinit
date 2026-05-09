656af92494527aecfa09b58de2e223e2daaf4d87
Xavier Hurtado Picón <xavihp84@gmail.com>
2026-05-09 18:38:29 +0200

    fix: cerca al vol funcional (sugereix correctament)

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ include_once __DIR__ . '/includes/header.php';
 
 <form action="api/cercaVol.php" method="POST" id="cerca-form">
     <textarea name="cerca" id="cerca" placeholder="🔍 Comença a buscar..."></textarea>
-    <input type="text" id="search">
     <ul id="suggestions"></ul>
     <button type="submit">Enviar</button>
 </form>
@@ -27,26 +26,19 @@ include_once __DIR__ . '/includes/header.php';
     input.addEventListener("input", async () => {
         const consulta = input.value;
 
-        if (consulta.length < 2) return; // evitar spam
-
         const res = await fetch(`/api/cercaVol.php?busqueda=${consulta}`);
         const data = await res.json();
 
-        mostrarSugerencies(data);
+        await mostrarSugerencies(data);
     });
 
-    function mostrarSugerencies(items) {
+    async function mostrarSugerencies(items) {
         const ul = document.getElementById("suggestions");
         ul.innerHTML = "";
 
         items.forEach(item => {
             const li = document.createElement("li");
-            li.textContent = item.title;
-
-            li.addEventListener("click", () => {
-                document.getElementById("cerca").value = item.title;
-                ul.innerHTML = "";
-            });
+            li.textContent = item;
 
             ul.appendChild(li);
         });
